6db4cfe3c067a5c7659c560655d12ad216462784
jsn.crdnl <jsn.crdnl@gmail.com>
2025-04-04 13:46:01 +0200

    fixed generate.sh line endings & fixed generate.bat dir creation

--- generate.bat
+++ generate.bat
@@ -1,3 +1,3 @@
-mkdir audios
+if not exist audios mkdir audios
 docker build -t tts-generator .
 docker run --rm -v.\audios:/app/audios -v .\input.txt:/app/input.txt tts-generator
--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-mkdir audios
+mkdir -p audios
 docker build -t tts-generator .
-docker run --rm -v./audios:/app/audios -v ./input.txt:/app/input.txt tts-generator
+docker run --rm -v ./audios:/app/audios -v ./input.txt:/app/input.txt tts-generator
